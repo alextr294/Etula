584e73a1e07c9da6744baf5793be6fcdf2f19d6b
DUMONT MATHEO p1602557 <matheo.dumont@etu.univ-lyon1.fr>
2019-10-25 13:10:36 +0200

    Revert "Merge branch 'CI' into 'master'"
    This reverts merge request !1

--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Tests\Feature;
+namespace Tests\Unit;
 
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\RefreshDatabase;
@@ -14,9 +14,6 @@ class ExampleTest extends TestCase
      */
     public function testBasicTest()
     {
-        /*$response = $this->get('/');
-
-        $response->assertStatus(200);*/
-	$this->assertContains(4, [1, 4, 3]);
+        $this->assertTrue(true);
     }
 }
